93ab94b7cc916e94364529d5cd7e82d1dd7a3d5e
Karanpreet2001 <karanpreetfarwaha@gmail.com>
2022-05-14 03:39:13 -0700

    .

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -17,6 +17,10 @@ const Intro = () => {
     console.log(container);
   };
 
+  const toGitHub=()=>{
+      const link = 'https://github.com/Karanpreet2001';
+      window.open(link, '_blank');
+  }
    
   
 
@@ -27,108 +31,34 @@ const Intro = () => {
 
      
        <div className="intro" id='intro'>
-       
-       <div className="left"><div className='imgContainer'><img src="https://www.seekpng.com/png/full/76-760594_man-transparent-resolution-smile-man-png.png" alt="" /></div></div>
        <div className="right">
-       <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        
-        fpsLimit: 100,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          number: {
-            value: 200,
-            density: {
-              enable: true,
-              value_area: 7000
-            }
-          },
-          position:{
-            value:"absolute"
-          },
-          size: {
-            value: 12,
-            random: true,
-            anim: {
-              speed: 5,
-              size_min: 0.3
-            }
-          },
-          zIndex:{
-            value:-1
-          },
-          opacity: {
-            value: 0.5,
-          },
-          
-          color: {
-            value: ["#B0FC38", "#B2D3C2", "#FFFF00"]
-          },
-          shape: {
-            type: ["circle", "edge", "polygon"]
-          },
-          move: {
-            
-            direction: "top",
-            enable: true,
-            outModes: {
-              default: "bounce",
-            },
-            random: true,
-            speed: 3,
-            
-          },
-          line_linked: {
-            enable: false
-          }
-        },
-        detectRetina: true,
-      }}
-    />
+      
        
-           <div className="wrapper">
-               <h2>Hi there, I'm</h2>
-               <h1>Karanpreet</h1>
-               <h3><span><Typewriter 
-               
-               options={
-                   { strings:["Full Stack Developer", "Web Developer", "Android Developer", "Java Developer", "MERN Stack Developer"],
+      <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Karanpreet</h1>
+          <h3><span><Typewriter 
+          
+          options={
+              { strings:["Full Stack Developer", "Web Developer", "Android Developer", "Java Developer", "MERN Stack Developer"],
 
-                autoStart:true,
-                delay:75,
-            loop:true}}
-               
-               /> </span></h3>
-           </div>
-           <a href='#portfolio'></a>
-       </div>
+           autoStart:true,
+           delay:75,
+       loop:true}}
+          
+          /> </span></h3>
+
+          <button onClick={()=>toGitHub()}>Check All my projects</button>
+         
+      </div>
+     
+     
+  </div>
+       <div className="left"><div className='imgContainer'><img src="https://www.seekpng.com/png/full/76-760594_man-transparent-resolution-smile-man-png.png" alt="" /></div></div>
+       
     </div> 
   
-    );
+                 );
 }
  
 export default Intro;
